f78b14b67225c529cd270a165cde3a6ad23b64c9
unknown <c.tsai1027@gmail.com>
2020-09-26 12:51:16 +0800

    Add 2

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
     <input v-model="text" />
-    <button @click="add" :disabled ="!text">
-        Add
-    </button>
+    <button @click="add" :disabled="!text">Add</button>
     <ul>
-      <li v-for="item in list" :key="item">
-        {{ item }}
+      <li v-for="item in list" :key="item.text">
+        <input type="checkbox" v-model="item.done" />
+        {{ item.text }}
       </li>
     </ul>
+    <pre>
+        {{ JSON.stringify(list, null, 2) }}
+    </pre>
   </div>
 </template>
 
@@ -16,13 +18,21 @@
 export default {
   data: () => ({
     text: "",
-    list: ["a", "b", "c"],
+    list: [],
   }),
   methods: {
     add() {
-        if(!this.text) return
-        this.list.push(this.text)
-        this.text = "";
+      if (!this.text) return;
+      const item = {
+        text: this.text,
+        done: false,
+      };
+      this.list.push(item);
+      this.text = "";
+    },
+    remove(item) {
+      const idx = this.list.indexOf(item);
+      this.list.splice(idx, 1);
     },
   },
 };
