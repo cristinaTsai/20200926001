089634fe95031d278a1e70409b96c18aba2a586e
unknown <c.tsai1027@gmail.com>
2020-09-26 16:14:15 +0800

    add zip2

--- src/components/CityCafe.vue
+++ src/components/CityCafe.vue
@@ -1,15 +1,17 @@
 <template>
   <div>
-      {{cityCode}}
+    {{ cityCode }}
     <select v-model="cityCode">
       <option v-for="city in cities" :key="city.code" :value="city.code">
         {{ city.name }}
       </option>
     </select>
-     <select v-model="regionCode">
-       <option v-for="region in regions" :key="region.code" :value="region.code">
+    <br />
+    {{ regionCode }}
+    <select v-model="regionCode">
+      <option v-for="region in regions" :key="region.code" :value="region.code">
         {{ region.name }}
-      </option>       
+      </option>
     </select>
     <!--     <pre>
         {{JSON.stringify(zip, null, 2)}}
@@ -26,13 +28,23 @@ export default {
   data: () => ({
     zip,
     cityCode: 100,
+    regionCode: 0,
   }),
   computed: {
     cities() {
       return this.zip.cities;
     },
     regions() {
-      return [];
+      const city = this.cities.find((elm) => elm.code === this.cityCode);
+      return city.region;
+    },
+  },
+  watch: {
+    cityCode: {
+      handler() {
+        this.regionCode = this.regions[0].code;
+      },
+      immediate: true,
     },
   },
 };
